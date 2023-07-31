c8d97ead952b92727c68ce7b5dc7bbedcda06240
Ali Messaoudi <83890004+MESSDEV@users.noreply.github.com>
2023-07-31 16:59:13 +0200

    Create takeDamage.lua

---
+++ takeDamage.lua
@@ -0,0 +1,12 @@
+local fire_on = true
+function onTouchBam(who)
+  if fire_on == true and who.Parent.Humanoid then
+    fire_on = false
+    if script.Parent.Fire then
+       who.Parent.Humanoid:TakeDamage(20)
+       wait(0.2)
+       fire_on = true
+    end
+  end
+end
+script.Parent.Touched:Connect(onTouchBam)
